4ab07dbda34d84cf2dd768884288597ba14b6c8e
Laura Keidann Rodrigues da Silva <82679218+laurakrs@users.noreply.github.com>
2026-08-25 19:01:24 +0000

    Clear menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,3 @@ st.title("Study Planner")
 st.write(
     "Let's go!"
 )
-
-
-st.sidebar.title("Menu")
-page = st.sidebar.radio(
-    "Go to",
-    ["Dashboard", "Pomodoro Timer", "Task Tracker", "Study Log", "Analytics"],
-)
-
